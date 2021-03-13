b83d93d4c0dbfecb60f2cb0cad0121e6ee99a127
law-pal <lawrencepalacios1@gmail.com>
2021-03-12 19:19:05 -0600

    updated README file

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,6 +4,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com
 export const authConfig = {
   domain: 'lawrencep.us.auth0.com',          
   clientId: 'ZL9l8DiOKsl90N5wLlRtOxHKYCtG9AMG',        
-  callbackUrl: 'http://localhost:3000/callback'
-  //callbackUrl: 'http://172.26.143.172:3000/callback'
+  //callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://172.26.143.172:3000/callback'
 }
